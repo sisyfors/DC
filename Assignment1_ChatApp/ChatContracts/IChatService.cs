e5b3b86ef35e90671d3cb47de5a036e4c889b942
sisyfors <ethandev97@gmail.com>
2026-08-16 06:02:40 +0000

    1st

---
+++ Assignment1_ChatApp/ChatContracts/IChatService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ChatContracts
+{
+    [ServiceContract]
+    public interface IChatService
+    {
+        [OperationContract]
+        string TestConnection();
+    }
+}
